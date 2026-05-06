2cf93d7167315f964caf365340b4357135e94f76
Eirik Igland <github@rabben.io>
2026-05-06 12:00:56 +0200

    feat: Update frontend dependencies

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,5 @@
 import "@css/app.css";
 
-// Put Alpine last, so that it has access to other JS modules
 import Alpine from "alpinejs";
 window.Alpine = Alpine;
 Alpine.start();
-
-if (import.meta.hot) {
-  import.meta.hot.accept(() => {
-    console.log("HMR");
-  });
-}
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,16 +1,13 @@
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import manifestSRI from "vite-plugin-manifest-sri";
-import path from "path";
-import viteCompression from "vite-plugin-compression";
 import ViteRestart from "vite-plugin-restart";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
   base: command === "serve" ? "" : "/dist/",
   build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
     manifest: true,
     outDir: path.resolve(__dirname, "web/dist/"),
     rollupOptions: {
@@ -23,10 +20,8 @@ export default defineConfig(({ command }) => ({
     },
   },
   plugins: [
+    tailwindcss(),
     manifestSRI(),
-    viteCompression({
-      filter: /\.(js|mjs|json|css|map)$/i,
-    }),
     ViteRestart({
       reload: ["templates/**/*"],
     }),
